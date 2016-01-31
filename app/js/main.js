222ca6ee94aedeac29d3540863d43e7473df57b3
Viktor Plakyda <plakyda1@gmail.com>
2016-02-01 01:33:16 +0200

    bug fixed,add autoriz, px-to-%

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
 	console.log('Its OK')
 	jQuery('input[placeholder], textarea[placeholder]').placeholder();
+// $(window).resize(function(){
+// 	var width = $(window).width();
+// 	if(width > 900 ){
+// 		$('#menu').detach().prependTo('#div');
+// 	}
+// })
+
 })
 
